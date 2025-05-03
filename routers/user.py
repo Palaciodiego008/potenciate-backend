0aec178b7f7fdfcadb860dbaa175fd5235c0cf37
AnaFSALAS <90934751+AnaFSALAS@users.noreply.github.com>
2025-05-03 11:07:29 -0500

    implement auth

--- routers/user.py
+++ routers/user.py
@@ -1,1 +1,20 @@
 print("user router")
+
+from pydantic import BaseModel, EmailStr
+
+# Usuario para registrar
+class UserCreate(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+# Usuario para login
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
+# Usuario en base de datos
+class User(BaseModel):
+    username: str
+    email: EmailStr
+    hashed_password: str
